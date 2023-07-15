972c1e3f5e51ea579376741a84b96fe1ed3da581
starryeye <see2818@naver.com>
2023-07-16 03:23:40 +0900

    [starryeye, 2023.07.16] - 주석 추가
    내용
    - 주석 추가

--- coupon/api/src/main/java/dev/practice/api/service/ApplyServiceWithRedis.java
+++ coupon/api/src/main/java/dev/practice/api/service/ApplyServiceWithRedis.java
@@ -25,6 +25,14 @@ public class ApplyServiceWithRedis {
          * - Redis 에서 관리하여 DB 의 totalCount 쿼리에 대한 부담이 없어졌다. (메모리 DB 라 빠르기도함)
          * - Redis 의 INCR 명령어는 단일 쓰레드로 동작하기 때문에 count 에 대한 동시성 문제가 발생하지 않는다.
          * - 결론적으로, count 에 대해 동시성 문제와 totalCount 쿼리 두가지를 동시에 해결한 것이다.
+         *
+         * 문제점
+         * - 일반적으로 DB 는 coupon 발급에 해당하는 데이터만을 다루진 않는다.
+         * - 예를 들어 쿠폰 발급 저장이 1분이 걸린다고 가정하면..
+         * - DB 커넥션이 10 개이고 쿠폰 발급 요청이 1000 개가 들어온다면,
+         * - 약 100분에 가까운 시간이 소요된다.. (쿠폰 발급에 대한 timeout 발생도 가능하다.. redis 에서 걸리는 시간은 O(1) 이므로 무시)
+         * - 그리고.. 쿠폰 발급외의 다른 비즈니스 로직은 수행되지 못하고 대기하거나 timeout 이 발생하게 된다.
+         * - 결론적으로, tomcat 스레드 풀 또는 DB 커넥션 풀이 모두 고갈되어 서비스가 불가능한 상태가 된다.
          */
 
         long count = couponCountRepository.increment();
