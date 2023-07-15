3026a56b44cfc75e02528c805c0f6320b0c515de
starryeye <see2818@naver.com>
2023-07-16 02:03:42 +0900

    [starryeye, 2023.07.16] - coupon service test 작성
    내용
    - 쿠폰 발급 요청이 여러개 들어오면 100개를 초과하는 쿠폰이 발급된다.

---
+++ coupon/api/src/test/java/dev/practice/api/service/ApplyServiceTest.java
@@ -0,0 +1,70 @@
+package dev.practice.api.service;
+
+import dev.practice.api.repository.CouponRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ApplyServiceTest {
+
+    @Autowired
+    private ApplyService applyService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    // 기본 기능 Test, Local 테스트로 ddl-auto 를 create 로 설정 필요
+    @Test
+    void applyCoupon() {
+        //given
+        long userId = 1L;
+
+        //when
+        applyService.applyCoupon(userId);
+
+        //then
+        long count = couponRepository.count();
+        Assertions.assertThat(count).isEqualTo(1L);
+    }
+
+    /**
+     * count 에 대한 race condition test 이다.
+     *
+     * 동일한 userId 에 대한 race condition 은 해당 프로젝트에서 다루지 않는다.
+     */
+    @Test
+    void concurrencyTest() throws InterruptedException {
+
+        int threadCount = 1000;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for(int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.applyCoupon(userId);
+                }finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+
+        long count = couponRepository.count();
+
+        Assertions.assertThatThrownBy(
+                () -> Assertions.assertThat(count).isEqualTo(100L)
+        ).isInstanceOf(AssertionError.class); // 100 개를 초과하는 쿠폰이 발급된다.
+    }
+}
